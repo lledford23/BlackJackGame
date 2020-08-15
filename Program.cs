f7b990a47981b9ae2671678c600c851510d506c1
Lindsay Ledford <lindsayledford0523@gmail.com>
2020-08-15 08:37:08 -0400

    commit 2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BlackJackGame
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to BlackJack! What's your name?");
+            var playerOne = Console.ReadLine();
+
+            Console.WriteLine($"Nice to meet you, {playerOne}!");
+
+            var deck = new List<string>();
+            var suitList = new List<string>() { "Clubs", "Spades", "Diamonds", "Hearts" };
+            var rankList = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "King", "Queen", "Jack" };
+
+
+            for (var indexOfSuitList = 0; indexOfSuitList < suitList.Count(); indexOfSuitList++)
+            {
+
+                var suit = suitList[indexOfSuitList];
+
+                for (var indexOfRankList = 0; indexOfRankList < rankList.Count(); indexOfRankList++)
+                {
+
+                    var rank = rankList[indexOfRankList];
+
+
+                    deck.Add($"{rank} of {suit}");
+
+                }
+            }
+        }
     }
-  }
 }
